39015c8c0cae453f04584272a3b45d6f7a428d71
abismbirk <abismbirk@github.com>
2026-05-07 06:00:41 +0300

    Simplify parasite to virtual display only

--- app/src/main/java/com/alol/overlay/SystemUIParasite.java
+++ app/src/main/java/com/alol/overlay/SystemUIParasite.java
@@ -4,27 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
-import android.media.Image;
-import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
 public class SystemUIParasite {
     private MediaProjection mediaProjection;
     private VirtualDisplay maelstromDisplay;
-    private ImageReader imageReader;
-    private Handler backgroundHandler;
     private int width, height, density;
 
     public SystemUIParasite(Context context, Intent data) {
-        HandlerThread thread = new HandlerThread("Maelstrom");
-        thread.start();
-        backgroundHandler = new Handler(thread.getLooper());
-
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics metrics = new DisplayMetrics();
         wm.getDefaultDisplay().getRealMetrics(metrics);
@@ -35,23 +25,13 @@ public class SystemUIParasite {
         MediaProjectionManager mpManager = (MediaProjectionManager) context.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
         mediaProjection = mpManager.getMediaProjection(-1, data);
 
-        imageReader = ImageReader.newInstance(width, height, android.graphics.PixelFormat.RGBA_8888, 2);
-        imageReader.setOnImageAvailableListener(reader -> {
-            Image image = reader.acquireLatestImage();
-            if (image != null) {
-                // هنا سيعمل الذكاء الاصطناعي لاحقاً
-                image.close();
-            }
-        }, backgroundHandler);
-
         maelstromDisplay = mediaProjection.createVirtualDisplay("Maelstrom", width, height, density,
                 DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                imageReader.getSurface(), null, backgroundHandler);
+                null, null, null);
     }
 
     public void stop() {
         if (maelstromDisplay != null) maelstromDisplay.release();
         if (mediaProjection != null) mediaProjection.stop();
-        if (imageReader != null) imageReader.close();
     }
 }
