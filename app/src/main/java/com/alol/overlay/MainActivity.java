ca96091a0c254bdcb60348de8a903020edd5b09f
abismbirk <abismbirk@github.com>
2026-05-07 05:50:28 +0300

    Add crash reporting dialog

--- app/src/main/java/com/alol/overlay/MainActivity.java
+++ app/src/main/java/com/alol/overlay/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alol.overlay;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,6 +14,26 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // اعتراض أي كراش وعرضه
+        final Thread.UncaughtExceptionHandler oldHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(e.toString() + "\n\n");
+                for (StackTraceElement ste : e.getStackTrace()) {
+                    sb.append(ste.toString() + "\n");
+                }
+                new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Crash Report")
+                    .setMessage(sb.toString())
+                    .setPositiveButton("OK", (d, w) -> System.exit(1))
+                    .show();
+                if (oldHandler != null) oldHandler.uncaughtException(t, e);
+            }
+        });
+
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setGravity(Gravity.CENTER);
@@ -26,7 +47,20 @@ public class MainActivity extends Activity {
         startBtn.setText("Activate Phantom");
         startBtn.setOnClickListener(v -> {
             if (parasite == null) {
-                parasite = new SystemUIParasite(this);
+                try {
+                    parasite = new SystemUIParasite(this);
+                } catch (Exception e) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(e.toString() + "\n\n");
+                    for (StackTraceElement ste : e.getStackTrace()) {
+                        sb.append(ste.toString() + "\n");
+                    }
+                    new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Error")
+                        .setMessage(sb.toString())
+                        .setPositiveButton("OK", null)
+                        .show();
+                }
             }
         });
         layout.addView(startBtn);
